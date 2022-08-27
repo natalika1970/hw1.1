77435d9f0e99f3e5013f4d7bb02580590a4c29f4
natalika1970 <natalika_70@mail.ru>
2022-08-27 23:43:02 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // переменные задача 1
+        System.out.println("переменные задача 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // переменные задача 2
+        System.out.println("переменные задача 2");
+
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        // переменные задача 3
+        System.out.println("переменные задача 3");
+
+        var dog1 = 12.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        // переменные задача 4
+        System.out.println("переменные задача 4");
+
+
+
+
+
+
     }
 }
